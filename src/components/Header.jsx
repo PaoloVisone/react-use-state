0a5ff3c92b84dc24f5c66d3e739041d37fdebde3
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-17 21:27:07 +0100

    [feat] add title

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (<header>
+
+        {/* Title */}
+        <div id="title">
+            <h1>Learn Web development</h1>
+        </div>
+
+    </header>);
+}
