ce327a80525d8170a3e8826fa0396fa0df910dca
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-09 23:12:59 +0100

    [fix] move the use state to the parent component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,26 @@
 import languages from "/languages.js";
 import Button from "./Button";
 
+import { useState } from "react";
+
 export default function Card() {
+
+    const [active, setActive] = useState(null)
 
     // Funzione di rendering
     function renderCard() {
         // Ciclo 
         return languages.map(language => {
             return <Button
+
                 // Passo le props
                 key={language.id}
                 titolo={language.title}
                 descrizione={language.description}
+                // Setto lo stato in base all'id
+                isOpen={active === language.id}
+                onToggle={() => setActive(language.id)}
+
             />
 
         });
@@ -23,6 +32,7 @@ export default function Card() {
             {/* Chiamo la funzione di rendering */}
             {renderCard()}
         </div>
+
     )
 
 }
